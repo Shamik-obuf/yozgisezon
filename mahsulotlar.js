37d8fa5ba53ff0f62dcff418a204dda21bf15b90
Shamik-obuf <murodovximik95@gmail.com>
2025-06-28 01:20:36 +0500

    Add files via upload

---
+++ mahsulotlar.js
@@ -0,0 +1,24 @@
+
+const mahsulotlar = [
+  { id: 1, nom: "Mekomeelo", narx: 260ming, razmer: "40-44", rasm: "Mahsulot1.jpg" },
+  { id: 2, nom: "Mahsulot 2", narx: 240ming, razmer: "M/L", rasm: "Mahsulot2.jpg" },
+  { id: 3, nom: "Mahsulot 3", narx: 260ming, razmer: "M/L", rasm: "Mahsulot3.jpg" },
+  { id: 4, nom: "Mahsulot 4", narx: 260ming, razmer: "M/L", rasm: "Mahsulot4.jpg" },
+  { id: 5, nom: "Mahsulot 5", narx: 260ming, razmer: "M/L", rasm: "Mahsulot6.jpg" }
+  { id: 6, nom: "Mekomeelo", narx: 260ming, razmer: "40-44", rasm: "Mahsulot7.jpg" },
+  { id: 7, nom: "Mahsulot 2", narx: 260ming, razmer: "M/L", rasm: "Mahsulot2.jpg" },
+  { id: 8, nom: "Mahsulot 3", narx: 260ming, razmer: "M/L", rasm: "Mahsulot3.jpg" },
+  { id: 9, nom: "Mahsulot 4", narx: 260ming, razmer: "M/L", rasm: "Mahsulot4.jpg" },
+  { id: 10, nom: "Mahsulot 5", narx: 260ming, razmer: "M/L", rasm: "Mahsulot5.jpg" }
+  { id: 11, nom: "Mekomeelo", narx: 260ming, razmer: "40-44", rasm: "Mahsulot1.jpg" },
+  { id: 12, nom: "Mahsulot 2", narx: 260ming, razmer: "M/L", rasm: "Mahsulot2.jpg" },
+  { id: 13, nom: "Mahsulot 3", narx: 260ming, razmer: "M/L", rasm: "Mahsulot3.jpg" },
+  { id: 14, nom: "Mahsulot 4", narx: 260ming, razmer: "M/L", rasm: "Mahsulot4.jpg" },
+  { id: 15, nom: "Mahsulot 5", narx: 260ming, razmer: "M/L", rasm: "Mahsulot5.jpg" }
+  { id: 16, nom: "Mekomeelo", narx: 260ming, razmer: "40-44", rasm: "Mahsulot1.jpg" },
+  { id: 17, nom: "Mahsulot 2", narx: 260ming, razmer: "M/L", rasm: "Mahsulot2.jpg" },
+  { id: 18, nom: "Mahsulot 3", narx: 260ming, razmer: "M/L", rasm: "Mahsulot3.jpg" },
+  { id: 19, nom: "Mahsulot 4", narx: 260ming, razmer: "M/L", rasm: "Mahsulot4.jpg" },
+  { id: 20, nom: "Mahsulot 5", narx: 260ming, razmer: "M/L", rasm: "Mahsulot5.jpg" }
+
+];
